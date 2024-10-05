9113f9a4ed80cf04876f0570eeac0929cd7383fc
Yohanes Kevin <yohanes123kevin123@gmail.com>
2024-10-05 19:59:07 +0700

    optimization main and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
-import 'config/theme/theme.dart';
-import 'config/routes.dart';
+import './config/theme/theme.dart';
+import './presentation/pages/common/splash.dart';
+import './config/routes.dart';
+
 
 void main() {
-  runApp(MyApp());
+  setupServiceLocator();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Bimbingan',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: AppRoutes.generateRoute,
+      home: const SplashPage(),
     );
   }
 }
