659e1ad0b2ac5d7639d2fabce3c7aa57c3279668
Yohanes Kevin <yohanes123kevin123@gmail.com>
2024-10-06 09:56:48 +0700

    dio_client -> api_helper, moving network to utils

--- lib/utils/api_helper.dart
+++ lib/utils/api_helper.dart
@@ -1,139 +1,110 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
-class ApiHelper {
-  static const String baseUrl = 'YOUR_BASE_URL';
-  static String? _authToken;
+import 'interceptors.dart';
 
-  // Setter untuk token
-  static void setToken(String token) {
-    _authToken = token;
-  }
+class DioClient {
+  
+  late final Dio _dio;
+  DioClient()
+      : _dio = Dio(
+          BaseOptions(
+              headers: {'Content-Type': 'application/json; charset=UTF-8'},
+              responseType: ResponseType.json,
+              sendTimeout: const Duration(seconds: 10),
+              receiveTimeout: const Duration(seconds: 10)),
+        )..interceptors.addAll([LoggerInterceptor()]);
 
-  // Getter untuk headers dengan authentication
-  static Map<String, String> get _headers => {
-    'Content-Type': 'application/json',
-    if (_authToken != null) 'Authorization': 'Bearer $_authToken',
-  };
-
-  // GET request
-  static Future<Map<String, dynamic>> get(String endpoint) async {
+  // GET METHOD
+  Future<Response> get(
+    String url, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) async {
     try {
-      final response = await http.get(
-        Uri.parse('$baseUrl$endpoint'),
-        headers: _headers,
+      final Response response = await _dio.get(
+        url,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
       );
-      
-      return _handleResponse(response);
-    } catch (e) {
-      throw _handleError(e);
+      return response;
+    } on DioException {
+      rethrow;
     }
   }
 
-  // POST request
-  static Future<Map<String, dynamic>> post(String endpoint, {dynamic body}) async {
+  // POST METHOD
+  Future<Response> post(
+    String url, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl$endpoint'),
-        headers: _headers,
-        body: jsonEncode(body),
+      final Response response = await _dio.post(
+        url,
+        data: data,
+        options: options,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
       );
       
-      return _handleResponse(response);
+      return response;
     } catch (e) {
-      throw _handleError(e);
+      rethrow;
     }
   }
 
-  // PUT request
-  static Future<Map<String, dynamic>> put(String endpoint, {dynamic body}) async {
+  // PUT METHOD
+  Future<Response> put(
+    String url, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
     try {
-      final response = await http.put(
-        Uri.parse('$baseUrl$endpoint'),
-        headers: _headers,
-        body: jsonEncode(body),
+      final Response response = await _dio.put(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
       );
-      
-      return _handleResponse(response);
+      return response;
     } catch (e) {
-      throw _handleError(e);
+      rethrow;
     }
   }
 
-  // DELETE request
-  static Future<Map<String, dynamic>> delete(String endpoint) async {
+  // DELETE METHOD
+  Future<dynamic> delete(
+    String url, {
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+  }) async {
     try {
-      final response = await http.delete(
-        Uri.parse('$baseUrl$endpoint'),
-        headers: _headers,
+      final Response response = await _dio.delete(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
       );
-      
-      return _handleResponse(response);
+      return response.data;
     } catch (e) {
-      throw _handleError(e);
+      rethrow;
     }
   }
-
-  // Upload file
-  static Future<Map<String, dynamic>> uploadFile(String endpoint, List<int> fileBytes, String fileName) async {
-    try {
-      var request = http.MultipartRequest('POST', Uri.parse('$baseUrl$endpoint'));
-      
-      request.files.add(
-        http.MultipartFile.fromBytes(
-          'file',
-          fileBytes,
-          filename: fileName,
-        ),
-      );
-
-      request.headers.addAll(_headers);
-      
-      var streamedResponse = await request.send();
-      var response = await http.Response.fromStream(streamedResponse);
-      
-      return _handleResponse(response);
-    } catch (e) {
-      throw _handleError(e);
-    }
-  }
-
-  // Handle API response
-  static Map<String, dynamic> _handleResponse(http.Response response) {
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      return jsonDecode(response.body);
-    } else if (response.statusCode == 401) {
-      throw UnauthorizedException('Unauthorized access');
-    } else {
-      throw ApiException(
-        'API Error: ${response.statusCode}',
-        response.statusCode,
-      );
-    }
-  }
-
-  // Handle errors
-  static Exception _handleError(dynamic e) {
-    if (e is ApiException) return e;
-    return ApiException('Network error: ${e.toString()}', 0);
-  }
-}
-
-class ApiException implements Exception {
-  final String message;
-  final int statusCode;
-
-  ApiException(this.message, this.statusCode);
-
-  @override
-  String toString() => message;
-}
-
-class UnauthorizedException implements Exception {
-  final String message;
-
-  UnauthorizedException(this.message);
-
-  @override
-  String toString() => message;
 }
