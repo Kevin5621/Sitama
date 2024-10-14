bc9e4de52b914e81271c6e65f942d332a677bafe
Kevin5621 <yohanes123kevin123@gmail.com>
2024-10-14 10:00:20 +0700

    Create filter_jurusan.dart

---
+++ lib/presenstation/lecturer/home/widgets/filter_jurusan.dart
@@ -0,0 +1,112 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sistem_magang/core/config/themes/app_color.dart';
+
+class FilterTahunWidget extends StatefulWidget {
+  const FilterTahunWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FilterTahunWidget> createState() => _FilterTahunWidgetState();
+}
+
+class _FilterTahunWidgetState extends State<FilterTahunWidget> {
+  final List<String> items = [
+    'D3 - Informatika',
+    'D3 - Elektronika',
+    'D3 - Telekomunikasi',
+    'D3 - Listrik',
+    'D4 - Telekomunikasi',
+
+  ];
+  String? selectedValue;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2<String>(
+        isExpanded: true,
+        hint: Text(
+          'Jurusan',
+          style: TextStyle(
+            fontSize: 14,
+            color: AppColors.gray,
+          ),
+        ),
+        items: items
+            .map((item) => DropdownMenuItem(
+                  value: item,
+                  child: Text(
+                    item,
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ))
+            .toList(),
+        value: selectedValue,
+        onChanged: (value) {
+          setState(() {
+            selectedValue = value;
+          });
+        },
+        buttonStyleData: const ButtonStyleData(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          height: 40,
+          width: 150,
+        ),
+        dropdownStyleData: const DropdownStyleData(
+          maxHeight: 200,
+        ),
+        menuItemStyleData: const MenuItemStyleData(
+          height: 40,
+        ),
+        dropdownSearchData: DropdownSearchData(
+          searchController: textEditingController,
+          searchInnerWidgetHeight: 50,
+          searchInnerWidget: Container(
+            height: 50,
+            padding: const EdgeInsets.only(
+              top: 8,
+              bottom: 4,
+              right: 8,
+              left: 8,
+            ),
+            child: TextFormField(
+              expands: true,
+              maxLines: null,
+              controller: textEditingController,
+              decoration: InputDecoration(
+                isDense: true,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 8,
+                ),
+                hintText: 'Search for an item...',
+                hintStyle: const TextStyle(fontSize: 12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ),
+          searchMatchFn: (item, searchValue) {
+            return item.value.toString().toLowerCase().contains(searchValue.toLowerCase());
+          },
+        ),
+        onMenuStateChange: (isOpen) {
+          if (!isOpen) {
+            textEditingController.clear();
+          }
+        },
+      ),
+    );
+  }
+}
